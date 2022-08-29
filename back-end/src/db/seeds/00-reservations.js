9c341dc8a56fbba046c1de5453c8f5f75bb1f7d5
JB Burroughs <jbburroughs.dev@gmail.com>
2022-08-29 15:04:28 -0400

    migrate & seed development env

--- back-end/src/db/seeds/00-reservations.js
+++ back-end/src/db/seeds/00-reservations.js
@@ -1,3 +1,9 @@
+const data = require("./00-reservations.json")
+
 exports.seed = function (knex) {
   return knex.raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE");
 };
+
+exports.seed = function (knex) {
+  return knex("reservations").insert(data)
+}
