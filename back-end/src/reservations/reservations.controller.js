6df7dd8ab3a55645aa275d63d12a72c283cc67f5
JB Burroughs <jbburroughs.dev@gmail.com>
2022-08-30 15:02:05 -0400

    added hasRequiredProperties validator

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,4 +1,5 @@
-const service = require("./reservations.service")
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const service = require("./reservations.service");
 
 //TODO build validation for valid properties
 //TODO build specific validator for reservation_date, reservation_time, and people
@@ -8,14 +9,17 @@ const service = require("./reservations.service")
 /**
  * Validator to check if reservation exists
  */
-async function reservationExists(req, res, next){
-  const reservation = await service.read(req.params.reservation_id)
+async function reservationExists(req, res, next) {
+  const reservation = await service.read(req.params.reservation_id);
 
-  if (reservation){
+  if (reservation) {
     res.locals.reservation = reservation;
     return next();
   }
-  next({ status: 404, message: "Reservation cannot be found."})
+  next({
+    status: 404,
+    message: `Reservation ${req.params.reservation_id} cannot be found.`,
+  });
 }
 
 const VALID_PROPERTIES = [
@@ -24,33 +28,57 @@ const VALID_PROPERTIES = [
   "mobile_number",
   "reservation_date",
   "reservation_time",
-  "people"
-]
+  "people",
+];
 
 /**
- * Validator to check that each reservation contains all the valid properties
+ * Validator to check that each reservation contains only valid properties
  */
-async function hasOnlyValidProperties(req, res, next){
+async function hasOnlyValidProperties(req, res, next) {
   const { data = {} } = req.body;
 
-  const invalidFields = Object.keys(data).filter(field => !VALID_PROPERTIES.includes(field))
+  const invalidFields = Object.keys(data).filter((field) => {
+    !VALID_PROPERTIES.includes(field);
+  });
 
   if (invalidFields.length) {
     return next({
-      status: 400, 
-      message: `Invalid field(s): ${invalidFields.join(", ")}`
+      status: 400,
+      message: `Invalid field(s): ${invalidFields.join(", ")}`,
     });
   }
   next();
 }
 
 /**
+ * Validator to check that each reservation contains all the required properties
+ */
+async function hasRequiredProperties(req, res, next) {
+  const { data = {} } = req.body;
+
+  VALID_PROPERTIES.forEach((property) => {
+    // console.log("property", property)
+    if (!data[property]) {
+      return next({
+        status: 400,
+        message: `A '${property}' property is required.`,
+      });
+    }
+  });
+  next();
+}
+
+/**
  * Specific validator to check reservation_date is a date
  */
-// async function isValidDate(req, res, next) {
+async function isValidDate(req, res, next) {
+  const { data = {} } = req.body;
+  let date = data["reservation_date"]
 
-// }
-
+  let result = Date.parse(date)
+  console.log("date", data["reservation_date"], "time", data["reservation_time"], "people", data["people"])
+  console.log("result", result)
+}
 
 // HANDLERS
 
@@ -65,32 +93,22 @@ async function list(req, res) {
 
 function read(req, res) {
   res.json({
-    data: res.locals.reservation
-  })
+    data: res.locals.reservation,
+  });
 }
 
 async function create(req, res) {
-  const newReservation = await service.create(req.body.data)
-
-  res.status(201).json({ data: newReservation })
-
-
-
-
-  // const { data: {first_name, last_name, mobile_number, reservation_date, reservation_time, people} = {} } = req.body;
-  // const newReservation = {
-  //   first_name,
-  //   last_name,
-  //   mobile_number,
-  //   reservation_date,
-  //   reservation_time,
-  //   people
-  // }
-  
+  await service.create(req.body.data);
+  res.sendStatus(201);
 }
 
 module.exports = {
   list,
-  read: [reservationExists, read],
-  create: [hasOnlyValidProperties, create]
+  read: [asyncErrorBoundary(reservationExists), read],
+  create: [
+    asyncErrorBoundary(hasOnlyValidProperties),
+    asyncErrorBoundary(hasRequiredProperties),
+    asyncErrorBoundary(isValidDate),
+    asyncErrorBoundary(create),
+  ],
 };
--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -8,14 +8,17 @@
 //TODO return only reservations matching date query param
 
 const router = require("express").Router();
+const methodNotAllowed = require("../errors/methodNotAllowed");
 const controller = require("./reservations.controller");
 
 router.route("/")
     .get(controller.list)
     .post(controller.create)
+    .all(methodNotAllowed)
     
 
 router.route("/:reservation_id")
     .get(controller.read)
+    .all(methodNotAllowed)
 
 module.exports = router;
