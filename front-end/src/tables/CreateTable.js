daf0a95a5dc46649b1a743f0d718ddf424f2e662
JB Burroughs <jbburroughs.dev@gmail.com>
2022-09-14 15:57:47 -0400

    CreateTable component built

--- front-end/src/tables/CreateTable.js
+++ front-end/src/tables/CreateTable.js
@@ -1,10 +1,37 @@
+import { createTable } from "../utils/api";
 import TableForm from "./TableForm";
+import { useHistory } from "react-router-dom"
+
 
 export default function CreateTable() {
+    const history = useHistory();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+    
+        const form = event.currentTarget;
+        const formData = new FormData(form);
+    
+        const newTable = {
+            data: {
+                table_name: formData.get("table_name"),
+                capacity: Number(formData.get("capacity")),
+                reservation_id: formData.get("reservation_id"),
+                is_occupied: formData.get("is_occupied"),
+            }
+        }
+        await createTable(newTable)
+        history.push("/dashboard")
+    }
+
+    const handleCancel = (event) => {
+        history.goBack()
+    }
+
     return (
         <>
             <h1>Create a Table</h1>
-            <TableForm />
+            <TableForm handleSubmit={handleSubmit} handleCancel={handleCancel} />
         </>
     )
 }
